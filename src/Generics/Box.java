56d83a1246c57b3277a788d625ba7573e817fa32
Vadim Rybakov <vadimio9217@gmail.com>
2020-02-19 14:37:26 +0300

    lesson1

---
+++ src/Generics/Box.java
@@ -0,0 +1,56 @@
+package Generics;
+
+import java.util.ArrayList;
+
+public class Box <F extends Fruit>{
+    private float weight;
+    private ArrayList<F> fruits = new ArrayList<>();
+    private F fruit;
+    private int numberOfFruits;
+
+    public F getFruit() {
+        return fruit;
+    }
+
+    public int getNumberOfFruits() {
+        return numberOfFruits;
+    }
+
+    public void setNumberOfFruits(int numberOfFruits) {
+        this.numberOfFruits += numberOfFruits;
+    }
+
+    public Box(F fruit){
+        this.weight = 0;
+        this.fruit = fruit;
+        this.numberOfFruits = 0;
+    }
+
+    public void put(F fr){
+        if(fruit.getClass() == fr.getClass()) {
+            fruits.add(fr);
+            numberOfFruits++;
+        }
+        else throw new RuntimeException("choose the box with another type");
+    }
+
+    public float getWeight(){
+        if(fruit.getClass().getName() == "Generics.Apple")
+            return numberOfFruits * 1.0f;
+        else
+            return numberOfFruits * 1.5f;
+    }
+
+    public boolean compare(Box <?> box){
+        if(this.getWeight() == box.getWeight()) return true;
+        else return false;
+    }
+
+    public void toEmptyBox(Box <?> box){
+        if(this.getFruit().getClass() == box.getFruit().getClass()) {
+            box.setNumberOfFruits(numberOfFruits);
+            numberOfFruits = 0;
+        }
+        else throw new RuntimeException("Wrong type of box");
+    }
+}
---
+++ src/Generics/Solution.java
@@ -0,0 +1,63 @@
+package Generics;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Solution {
+
+    //1. Написать метод, который меняет два элемента массива местами.(массив может быть любого ссылочного типа);
+    static <V> void  swap(int firstIndex,int secondIndex, V[] array){
+        V temp = array[firstIndex];
+        array[firstIndex] = array[secondIndex];
+        array[secondIndex] = temp;
+    }
+
+    //2. Написать метод, который преобразует массив в ArrayList;
+    //Arrays.asList(T... a) - исходник и ArrayList взаимозависимы,
+    //Collections.addAll(ArrayList strList, T[] strArr) - исходник и ArrayList независимы друг от друга
+    static <T> ArrayList<T> arrayAsList(T[] array) {
+        ArrayList<T> list = new ArrayList<>();
+        for (int i = 0; i < array.length; i++) {
+            list.add(array[i]);
+        }
+        return list;
+    }
+
+    public static void main(String[] args) {
+        Integer[] nums = {5, 8, 9, 12, 3};
+        String[] strs = {"first", "second", "third"};
+        //Проверка 1 задания.
+        swap(1, 4, nums);
+        System.out.println(Arrays.toString(nums));
+        swap(0,2, strs);
+        System.out.println(Arrays.toString(strs));
+
+        //Проверка 2 задания.
+        System.out.println(arrayAsList(strs));
+
+        //Проверка 3 задания.
+        Apple a = new Apple();
+        Orange o = new Orange();
+        Box <Fruit> appleBox = new Box<>(a);
+        Box <Fruit> orangeBox = new Box<>(o);
+        appleBox.put(a);
+//        appleBox.put(o);
+        for (int i = 0; i < 10; i++) {
+            orangeBox.put(o);
+        }
+        System.out.println(orangeBox.getWeight());
+        for (int i = 0; i < 14; i++) {
+            appleBox.put(a);
+        }
+        System.out.println(appleBox.getWeight());
+        System.out.println(appleBox.compare(orangeBox));
+//        orangeBox.toEmptyBox(appleBox);
+        Box<Fruit> newOrBox = new Box<>(o);
+        for (int i = 0; i < 7; i++) {
+            newOrBox.put(o);
+        }
+        orangeBox.toEmptyBox(newOrBox);
+        System.out.println(orangeBox.getNumberOfFruits());
+        System.out.println(newOrBox.getNumberOfFruits());
+    }
+}
