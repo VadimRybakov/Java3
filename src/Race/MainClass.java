820098126c8b272706d8582426396d2b3a9533ce
Vadim Rybakov <vadimio9217@gmail.com>
2020-03-04 16:29:40 +0300

    synchronize tunnel

--- src/Race/MainClass.java
+++ src/Race/MainClass.java
@@ -1,21 +1,26 @@
 package Race;
 
+import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.Semaphore;
 
 public class MainClass {
     public static final int CARS_COUNT = 4;
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException, BrokenBarrierException {
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
-        Race race = new Race(new Road(60), new Tunnel(), new Road(40));
+        Semaphore s = new Semaphore(CARS_COUNT/2);
+        Race race = new Race(new Road(100), new Tunnel(s), new Road(70));
         Car[] cars = new Car[CARS_COUNT];
         CyclicBarrier cb = new CyclicBarrier(CARS_COUNT);
+
         for (int i = 0; i < cars.length; i++) {
             cars[i] = new Car(race, 20 + (int) (Math.random() * 10), cb);
         }
-        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+
         for (int i = 0; i < cars.length; i++) {
             new Thread(cars[i]).start();
         }
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
 
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
     }
--- src/Race/Tunnel.java
+++ src/Race/Tunnel.java
@@ -1,21 +1,27 @@
 package Race;
 
+import java.util.concurrent.Semaphore;
+
 public class Tunnel extends Stage {
-    public Tunnel() {
+    private Semaphore s;
+    public Tunnel(Semaphore s) {
         this.length = 80;
         this.description = "Тоннель " + length + " метров";
+        this.s = s;
     }
     @Override
     public void go(Car c) {
         try {
             try {
                 System.out.println(c.getName() + " готовится к этапу(ждет): " + description);
+                s.acquire();
                 System.out.println(c.getName() + " начал этап: " + description);
                 Thread.sleep(length / c.getSpeed() * 1000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             } finally {
                 System.out.println(c.getName() + " закончил этап: " + description);
+                s.release();
             }
         } catch (Exception e) {
             e.printStackTrace();
